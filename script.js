2e5cba5bedb68ceb43f66111eebeca2b6d88a561
Ahmad <ahmad@ahmd-asdy.local>
2023-03-11 22:05:33 +0330

    fix bugs

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const timerShow = document.getElementById("timer");
 const totalTime = Number(video.duration);
 
 timeBar.max = totalTime;
+timeBar.style.width = "100%";
 
 function play_pause() {
   video.style.opacity = 1;
